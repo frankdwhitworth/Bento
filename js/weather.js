a25368709bf18c3b09947abecf1526ab43c05e7c
Frank Whitworth <59878304+frankdwhitworth@users.noreply.github.com>
2021-01-30 17:50:42 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -9,11 +9,11 @@ weather.temperature = {
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '5784f64a812d535ef2f85bdb86e2c3f6';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 38.827092;
+  let longitude = -77.047779;
 
   getWeather(latitude, longitude);
 }
